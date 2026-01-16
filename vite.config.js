7e931079a30fbf318e53c3f5864b2fe5e2e40a08
miku8miku <52441374+miku8miku@users.noreply.github.com>
2026-01-16 22:24:32 +0800

    fix(build): 根据部署环境调整base

--- vite.config.js
+++ vite.config.js
@@ -67,7 +67,9 @@ export default defineConfig({
       enforce: 'post',
     },
   ].filter(Boolean),
-  base: '/', // 子域名部署使用根路径
+  // base: '/', // 子域名部署使用根路径
+  // 根据部署环境调整 base 路径
+  base: process.env.DEPLOY_ENV === 'esa' ? '/wallpaper-gallery/' : '/',
   // 注入全局变量
   define: {
     __APP_VERSION__: JSON.stringify(APP_VERSION),
